290623313ea81a5f35725fab905ffb694b545229
ukalto <maxi.gaber@gmail.com>
2021-01-20 00:15:13 +0100

    Day 10 second solution

--- src/Day10.java
+++ src/Day10.java
@@ -14,13 +14,15 @@ public class Day10 {
         int n = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        int count = 0;
-        while (n != 0) {
-            n = (n & (n << 1));
-            count++;
-        }
-
-        System.out.println(count);
+//        Solution 1
+//        int count = 0;
+//        while (n != 0) {
+//            n = (n & (n << 1));
+//            count++;
+//        }
+//        System.out.println(count);
+//        Solution 2
+        System.out.println(Arrays.stream(Integer.toBinaryString(n).split("0")).map(String::length).max(Integer::compareTo).get());
 
         scanner.close();
     }
